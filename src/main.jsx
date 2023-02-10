202261ea802ff0f2bcd0517866cd440be0d249df
miguelfiais <miguel.fiais10@gmail.com>
2023-02-10 12:44:36 -0300

    starting the project structure

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Router from './routes/routes'
+import GlobalStyles from './styles/globalStyles'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router />
+    <GlobalStyles />
+  </React.StrictMode>,
+)
