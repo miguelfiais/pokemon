f0f0f460603f8936f16e5fb2edb9a8b8ccc86513
miguelfiais <miguel.fiais10@gmail.com>
2023-02-10 23:43:32 -0300

    feat: configure pokemon screen and get evolution chain information

--- src/pages/Pokemon/index.jsx
+++ src/pages/Pokemon/index.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { Container } from './styles';
 
 const Pokemon = () => {
+
+  const baseUrl = "https://pokeapi.co/api/v2/pokemon/"
+
+  const {name} = useParams()
+  const [pokemon, setPokemon] = useState({})
+  const [avatar, setAvatar] = useState("")
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const {data} = await axios.get(`${baseUrl}${name}`)
+      setAvatar(data.sprites.other.dream_world.front_default)
+      setPokemon(data)
+      await axios.get(data.species.url).then(response => axios.get(response.data.evolution_chain.url).then(response => {
+        axios.get(`${baseUrl}${response.data.chain.evolves_to[0].evolves_to[0].species.name}`).then(response => console.log(response.data))
+      }))
+    }
+    getPokemon()
+  }, [])
+
   return (
-    <div>Pokemon</div>
+    <Container>
+      <img src={avatar} alt={pokemon.name} />
+      <p>{pokemon.name}</p>
+      {
+        pokemon.stats && pokemon.stats.map(item => (
+          <div key={uuidv4()}>
+            <p>{item.stat.name}: <span>{item.base_stat}</span></p>
+          </div>
+        ))
+      }
+    </Container>
   )
 }
 
